cabe9f0297c66970186e618f7467b6907c9d31e4
Muhammad Iqbal <ncsiqbal23@gmail.com>
2026-09-02 16:16:03 +0700

    fix: load database config from environment

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -1,26 +1,63 @@
-public array $default = [
-    'DSN'          => '',
-    'hostname'     => env('database.default.hostname', 'localhost'),
-    'username'     => env('database.default.username', ''),
-    'password'     => env('database.default.password', ''),
-    'database'     => env('database.default.database', ''),
-    'DBDriver'     => env('database.default.DBDriver', 'MySQLi'),
-    'DBPrefix'     => '',
-    'pConnect'     => false,
-    'DBDebug'      => ENVIRONMENT !== 'production',
-    'charset'      => 'utf8mb4',
-    'DBCollat'     => 'utf8mb4_general_ci',
-    'swapPre'      => '',
-    'encrypt'      => false,
-    'compress'     => false,
-    'strictOn'     => false,
-    'failover'     => [],
-    'port'         => (int) env('database.default.port', 3306),
-    'numberNative' => false,
-    'foundRows'    => false,
-    'dateFormat'   => [
-        'date'     => 'Y-m-d',
-        'datetime' => 'Y-m-d H:i:s',
-        'time'     => 'H:i:s',
-    ],
-];
+<?php
+
+namespace Config;
+
+use CodeIgniter\Database\Config;
+
+class Database extends Config
+{
+    /**
+     * The directory that holds the Migrations and Seeds directories.
+     */
+    public string $filesPath = APPPATH . 'Database' . DIRECTORY_SEPARATOR;
+
+    /**
+     * Lets you choose which connection group to use if no other is specified.
+     */
+    public string $defaultGroup = 'default';
+
+    /**
+     * The default database connection.
+     *
+     * @var array<string, mixed>
+     */
+    public array $default = [
+        'DSN'          => '',
+        'hostname'     => '',
+        'username'     => '',
+        'password'     => '',
+        'database'     => '',
+        'DBDriver'     => 'MySQLi',
+        'DBPrefix'     => '',
+        'pConnect'     => false,
+        'DBDebug'      => false,
+        'charset'      => 'utf8mb4',
+        'DBCollat'     => 'utf8mb4_general_ci',
+        'swapPre'      => '',
+        'encrypt'      => false,
+        'compress'     => false,
+        'strictOn'     => false,
+        'failover'     => [],
+        'port'         => 3306,
+        'numberNative' => false,
+        'foundRows'    => false,
+        'dateFormat'   => [
+            'date'     => 'Y-m-d',
+            'datetime' => 'Y-m-d H:i:s',
+            'time'     => 'H:i:s',
+        ],
+    ];
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->default['hostname'] = env('database.default.hostname', 'localhost');
+        $this->default['username'] = env('database.default.username', '');
+        $this->default['password'] = env('database.default.password', '');
+        $this->default['database'] = env('database.default.database', '');
+        $this->default['DBDriver'] = env('database.default.DBDriver', 'MySQLi');
+        $this->default['DBDebug'] = ENVIRONMENT !== 'production';
+        $this->default['port'] = (int) env('database.default.port', 3306);
+    }
+}
